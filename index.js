253ddf0fe7c3667a8a7622e1b18afc25b4e4a9aa
Valeriy Chupurnov <skoder@ya.ru>
2021-12-23 19:58:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var plugin = require("./plugin");
+var { ParameterType } = require("typedoc");
+
 module.exports.load = function(app) {
-  app.options.addDeclaration({ name: 'external-modulemap', short: 'em' });
+  app.options.addDeclaration({ name: 'external-modulemap', short: 'em', type:  ParameterType.Mixed });
 
   (new plugin.ExternalModuleMapPlugin()).initialize(app);
 };
